1053cb3ef7661066b484694f515008c1fe4360dd
pakut2 <kutny.pawel@gmail.com>
2021-06-07 18:31:44 +0200

    react setup

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <Fragment>
+      <h1>React</h1>
+    </Fragment>
+  );
+};
+
+export default App;
