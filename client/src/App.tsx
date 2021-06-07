c3449af0c5136c9b627c0ccbca52bc1622fcbedc
pakut2 <kutny.pawel@gmail.com>
2021-06-07 20:32:11 +0200

    redux store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,16 @@ import React, { Fragment } from "react";
 import Landing from "./components/layout/Landing";
 import "./App.css";
 
+import { Provider } from "react-redux";
+import store from "./store";
+
 const App = () => {
   return (
-    <Fragment>
-      <Landing />
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <Landing />
+      </Fragment>
+    </Provider>
   );
 };
 
