92e492e2a0637666f573dd61ca820b50a2fec0b6
pakut2 <kutny.pawel@gmail.com>
2021-06-07 14:10:12 +0200

    post msg route

--- routes/api/messages.js
+++ routes/api/messages.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
-// @route   GET api/messages
-// @desc    test route
+const Message = require("../../models/Message");
+
+// @route   POST api/messages
+// @desc    POST message
 // @access  public
+router.post(
+  "/",
+  check("message", "Message is required").not().isEmpty(),
+  async (req, res) => {
+    const errors = validationResult(req);
 
-router.get("/", (req, res) => {
-  res.send("test route");
-});
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { message } = req.body;
+
+    try {
+      const msg = new Message({
+        message,
+      });
+
+      await msg.save();
+      return res.json(msg);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,10 +5,14 @@ const app = express();
 
 connectDB();
 
+// Init middleware
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => {
   res.send("API running");
 });
 
+// Define routes
 app.use("/api/messages", require("./routes/api/messages"));
 
 const PORT = process.env.PORT || 5000;
